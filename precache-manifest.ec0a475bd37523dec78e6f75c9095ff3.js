b73ed81f6716a786a8e6b1d915e0af2d66609d34
noah the goodra <peterpan0413@live.com>
2019-08-04 22:23:09 -0500

    Updates

---
+++ precache-manifest.ec0a475bd37523dec78e6f75c9095ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916f4d0a52000466b5a0a15adaf98d60",
+    "url": "/HungerGamesTesting/index.html"
+  },
+  {
+    "revision": "bfcabd35ed431f119c70",
+    "url": "/HungerGamesTesting/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "203a19495a517b5a92fa",
+    "url": "/HungerGamesTesting/static/css/main.a582c8eb.chunk.css"
+  },
+  {
+    "revision": "bfcabd35ed431f119c70",
+    "url": "/HungerGamesTesting/static/js/2.e0f37971.chunk.js"
+  },
+  {
+    "revision": "203a19495a517b5a92fa",
+    "url": "/HungerGamesTesting/static/js/main.13aba3ed.chunk.js"
+  },
+  {
+    "revision": "4a8760328bc9453acb9a",
+    "url": "/HungerGamesTesting/static/js/runtime~main.d29c18b9.js"
+  }
+]);
